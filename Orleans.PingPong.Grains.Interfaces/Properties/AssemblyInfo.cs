d9356e2324913f1fa7223d5f2e556922b93b28dc
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2014-04-09 18:54:26 +0300

    Fix

--- Orleans.PingPong.Grains.Interfaces/Properties/AssemblyInfo.cs
+++ Orleans.PingPong.Grains.Interfaces/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Orleans.PingPong.Grains.Intrefaces")]
+[assembly: AssemblyTitle("Orleans.PingPong.Grains.Interfaces")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Orleans.PingPong.Grains.Intrefaces")]
+[assembly: AssemblyProduct("Orleans.PingPong")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
